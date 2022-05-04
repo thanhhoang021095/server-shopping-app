3ac445debb581c2a96df7b843b2c08cef149135a
thanhhoang021095 <thanhhoang021095@gmail.com>
2022-05-04 09:44:56 +0700

    fix env db

--- src/common/connectDatabase.ts
+++ src/common/connectDatabase.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
 mongoose.Promise = require('bluebird')
 export const connectDatabase = (): void => {
-  console.log(process.env.NODE_ENV);
-  const mongodbConnectUrl = process.env.NODE_ENV === 'dev'
+  const mongodbConnectUrl = process.env.ENV_NAME === 'dev'
     ? 'mongodb://localhost:27017/shopDemo'
     : `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URLDEV || process.env.DB_URL}/${process.env.DB_NAME}`
   mongoose.connect(mongodbConnectUrl
