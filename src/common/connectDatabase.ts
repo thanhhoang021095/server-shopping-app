ae897d8d66949c23791378de5b23d2f4b3d8fd9e
thanhhoang021095 <thanhhoang021095@gmail.com>
2022-05-05 10:35:55 +0700

    change DB_URL

--- src/common/connectDatabase.ts
+++ src/common/connectDatabase.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 mongoose.Promise = require('bluebird')
 export const connectDatabase = (): void => {
   const mongodbConnectUrl = process.env.ENV_NAME === 'dev'
-    ? 'mongodb://localhost:27017/shopping-online'
-    : `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URLDEV || process.env.DB_URL}/${process.env.DB_NAME}`
+    ? `mongodb://${process.env.DB_URL_DEV}/${process.env.DB_NAME}`
+    : `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL_DEV || process.env.DB_URL_PROD}/${process.env.DB_NAME}`
   mongoose.connect(mongodbConnectUrl
     , {
       useUnifiedTopology: true,
